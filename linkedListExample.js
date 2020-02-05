4033c9ae42c18d0e7d822eb949f37d86e354f338
Pallab Borah <pallab.borah@kdi.kongsberg.com>
2020-02-05 18:46:40 +0530

    creation of queue from linked list is updated

---
+++ linkedListExample.js
@@ -0,0 +1,33 @@
+let row = {front: null, rear: null};
+
+class Node {
+    constructor(data,next)
+  {this.data = data;
+  this.next = next;}
+} 
+
+function Entry(element) {
+  var person = new Node(element, null);
+  if (row.rear === null) {
+    row.front = person;
+    row.rear = person; 
+  } else { 
+      
+    row.rear.next = person; 
+    row.rear = row.rear.next;
+  } 
+} 
+
+ 
+
+let size= prompt("enter the size:");
+
+
+for (let i=0;i<size;i++){
+Entry(prompt("enter the value:"));
+console.log(row.rear);
+}
+
+console.log("the front element in the queue is :"+row.front);
+
+console.log("the end element of the queue is :"+row.rear);
